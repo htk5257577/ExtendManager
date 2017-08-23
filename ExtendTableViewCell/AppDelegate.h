9e2a33c8dc92806721808659792ceadf66d23f19
htk5257577 <280071019@qq.com>
2017-08-23 13:49:14 +0800

    Initial Commit

---
+++ ExtendTableViewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExtendTableViewCell
+//
+//  Created by 胡焘坤 on 2017/8/23.
+//  Copyright © 2017年 胡焘坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExtendTableViewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExtendTableViewCell
+//
+//  Created by 胡焘坤 on 2017/8/23.
+//  Copyright © 2017年 胡焘坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
